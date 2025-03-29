4020bffdc8bbcda11caa4caac367ebd9e38e3fbb
jjunhaa0211 <goodjunha@gmail.com>
2025-03-29 11:09:43 +0900

    Initial Commit

---
+++ XhXComponent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XhXComponent
+//
+//  Created by 박준하 on 3/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ XhXComponentTests/XhXComponentTests.swift
@@ -0,0 +1,17 @@
+//
+//  XhXComponentTests.swift
+//  XhXComponentTests
+//
+//  Created by 박준하 on 3/29/25.
+//
+
+import Testing
+@testable import XhXComponent
+
+struct XhXComponentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
